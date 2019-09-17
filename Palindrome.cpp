4feb78e6ce34f970033f7129dc6bfa1fe0d93675
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-09-17 08:41:48 -0700

    first commit from school

---
+++ Palindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+  char palin[80];
+  char drome[80];
+  char palindrome[80];
+  int count = 0;
+  
+  cin.get(palin, 80);
+  cin.get();
+
+  for(int i = 0; i <= 80; i++){
+    if((palin[i] != ' ') && (ispunct(palin[i]) == false)){
+          drome[count] == tolower(palin[i]);
+	  count++;
+      }
+  }
+
+  cout << palin << endl;
+  cout << drome << endl;
+  
+}
