e8c064d8e606972d7429ec276d1a1b7010450f77
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-09-19 08:03:18 -0700

    working for loop to take out spaces and punctuation

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -14,12 +14,15 @@ int main(){
 
   for(int i = 0; i <= 80; i++){
     if((palin[i] != ' ') && (ispunct(palin[i]) == false)){
-          drome[count] == tolower(palin[i]);
+          drome[count] = tolower(palin[i]);
 	  count++;
       }
   }
 
   cout << palin << endl;
   cout << drome << endl;
-  
+
+  /* for(int j = 0; i < strlen(drome); i++){
+    
+     }*/
 }
